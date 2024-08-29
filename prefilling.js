42a8bd44d3d7c0a22cbee7207fcda281da0812de
Uzair Khalid <144005513+uzairdatabeys@users.noreply.github.com>
2024-08-29 20:08:10 +0500

    version 3

--- prefilling.js
+++ prefilling.js
@@ -1,14 +1,20 @@
-console.log("testing-1")
-var checkIframe = setInterval(function () {
-    console.log("testing-2")
-    const iframe = document.getElementById('iframe');
-    if (iframe) {
-        var iframeWindow = iframe.contentWindow;
-        if (iframeWindow) {
-            iframeWindow.postMessage({ utm_medium: "utmMedium", utm_source: "utmSource" }, "*");
-            clearInterval(checkIframe);
+document.addEventListener("DOMContentLoaded", function () {
+    var checkIframe = setInterval(function () {
+        const iframe = document.getElementById('iframe');
+        if (iframe) {
+            var iframeWindow = iframe.contentWindow;
+            if (iframeWindow) {
+                let currentLocation = window.location.href;
+                currentLocation = new URLSearchParams(currentLocation.split('?')[1]);
+                const utmMedium = currentLocation.get('utm_medium');
+                const utmSource = currentLocation.get('utm_source');
+                iframeWindow.postMessage({ utm_medium: utmMedium, utm_source: utmSource }, "*");
+                clearInterval(checkIframe);
+            } else {
+                console.error("IFrame contentWindow is null.");
+            }
         } else {
-            console.error("IFrame contentWindow is null.");
+            console.error("IFrame is null.");
         }
-    }
-}, 500)
+    }, 500)
+});
