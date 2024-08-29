b26e609b62f2a7bcbd234670c09354236940fb4f
Uzair Khalid <144005513+uzairdatabeys@users.noreply.github.com>
2024-08-29 16:37:28 +0500

    testing-v1

--- prefilling.js
+++ prefilling.js
@@ -1,15 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var checkIframe = setInterval(function(){
-        const iframe = document.getElementById('iframe');
-        if (iframe) {
-            var iframeWindow = iframe.contentWindow;
-            if (iframeWindow) {
-                iframeWindow.postMessage({ utm_medium: "utmMedium", utm_source: "utmSource" }, "*");
-                clearInterval(checkIframe);
-            }else{
-                console.error("IFrame contentWindow is null.");
-            }
+console.log("testing-1")
+var checkIframe = setInterval(function () {
+    console.log("testing-2")
+    const iframe = document.getElementById('iframe');
+    if (iframe) {
+        var iframeWindow = iframe.contentWindow;
+        if (iframeWindow) {
+            iframeWindow.postMessage({ utm_medium: "utmMedium", utm_source: "utmSource" }, "*");
+            clearInterval(checkIframe);
+        } else {
+            console.error("IFrame contentWindow is null.");
         }
-    }, 500)
-});
- 
+    }
+}, 500)
