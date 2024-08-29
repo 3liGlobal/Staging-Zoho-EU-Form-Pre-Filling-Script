e869a55a691ec3cc1e880011b078725d4ac6ac6f
Uzair Khalid <144005513+uzairdatabeys@users.noreply.github.com>
2024-08-29 20:11:42 +0500

    version 4

--- prefilling.js
+++ prefilling.js
@@ -1,9 +1,14 @@
+console.log("testing-1")
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("testing-2")
     var checkIframe = setInterval(function () {
+        console.log("testing-3")
         const iframe = document.getElementById('iframe');
         if (iframe) {
+            console.log("testing-4")
             var iframeWindow = iframe.contentWindow;
             if (iframeWindow) {
+                console.log("testing-5")
                 let currentLocation = window.location.href;
                 currentLocation = new URLSearchParams(currentLocation.split('?')[1]);
                 const utmMedium = currentLocation.get('utm_medium');
