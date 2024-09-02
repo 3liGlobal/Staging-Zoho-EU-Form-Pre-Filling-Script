c5f2d3c024b4ce88c42f84526df89096dae60782
Uzair Khalid <144005513+uzairdatabeys@users.noreply.github.com>
2024-09-02 17:04:04 +0500

    version 3

--- prefilling.js
+++ prefilling.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log(utmMedium, utmSource)
 
                 iframeWindow.postMessage({ utm_medium: utmMedium, utm_source: utmSource }, "*");
-                console.log("Message posted")
+                console.log(iframe)
                 clearInterval(checkIframe);
             } else {
                 console.error("IFrame contentWindow is null.");
