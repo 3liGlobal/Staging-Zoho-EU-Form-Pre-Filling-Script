f41d689f2f85bd9ed04ae618149afbf563d8f581
Uzair Khalid <144005513+uzairdatabeys@users.noreply.github.com>
2024-09-02 18:06:26 +0500

    v9

--- prefilling.js
+++ prefilling.js
@@ -1,50 +1,24 @@
 console.log("testing-1");
- 
 document.addEventListener("DOMContentLoaded", function () {
     var checkIframe = setInterval(function () {
         const iframe = document.getElementById('iframe');
         if (iframe) {
-            let currentLocation = window.location.href.split('#')[0];
-            let urlParams = new URL(currentLocation).searchParams;
- 
-            const utmMedium = urlParams.get('utm_medium');
-            const utmSource = urlParams.get('utm_source');
-            let attempts = 0;
-            let maxAttempts = 10; // Number of times to retry sending the message
- 
-            function sendMessage() {
-                if (attempts >= maxAttempts) {
-                    clearInterval(checkIframe);
-                    console.error("Max attempts reached. Message not acknowledged.");
-                    return;
+
+                let currentLocation = window.location.href.split('#')[0];
+                let urlParams = new URL(currentLocation).searchParams;
+
+                const utmMedium = urlParams.get('utm_medium');
+                const utmSource = urlParams.get('utm_source');
+
+                iframe.onload = function () {
+                    console.log("Inside")
+                    console.log(utmMedium, utmSource)
+                    iframe.contentWindow.postMessage({ utm_medium: utmMedium, utm_source: utmSource }, '*');
                 }
- 
-                console.log("Attempting to send message:", attempts + 1);
-                iframe.contentWindow.postMessage({ utm_medium: utmMedium, utm_source: utmSource }, '*');
-                attempts++;
-            }
- 
-            iframe.onload = function () {
-                console.log("Iframe loaded");
-                sendMessage();
- 
-                // Set up an interval to retry sending the message every 1 second
-                var retryInterval = setInterval(function () {
-                    sendMessage();
-                }, 1000);
- 
-                // Listen for acknowledgment from the child page
-                window.addEventListener('message', function (event) {
-                    if (event.data.acknowledged) {
-                        console.log("Message acknowledged by child page");
-                        clearInterval(retryInterval); // Stop retrying when acknowledged
-                    }
-                });
-            };
- 
-            clearInterval(checkIframe);
+
+                clearInterval(checkIframe);
         } else {
-            console.error("Iframe is null.");
+            console.error("IFrame is null.");
         }
     }, 500);
 });
