8e26af7b8ef52e10236424275c71f50703afdb92
Uzair Khalid <144005513+uzairdatabeys@users.noreply.github.com>
2024-09-03 11:04:34 +0500

    v1

--- prefilling.js
+++ prefilling.js
@@ -3,18 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var checkIframe = setInterval(function () {
         const iframe = document.getElementById('iframe');
         if (iframe) {
-
             let currentLocation = window.location.href.split('#')[0];
             let urlParams = new URL(currentLocation).searchParams;
 
             const utmMedium = urlParams.get('utm_medium');
             const utmSource = urlParams.get('utm_source');
 
-            iframe.onload = function () {
-                console.log("Inside")
-                console.log(utmMedium, utmSource)
-                iframe.contentWindow.postMessage({ utm_medium: utmMedium, utm_source: utmSource }, '*');
-            }
+            console.log(utmMedium, utmSource)
+            iframe.contentWindow.postMessage({ utm_medium: utmMedium, utm_source: utmSource }, '*');
 
             clearInterval(checkIframe);
         } else {
